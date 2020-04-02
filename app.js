7bd4dae41619fe6d7e0007e3a112547cf96375dd
neeerajtk <neerajtk560@gmail.com>
2020-04-02 15:23:32 +0530

    new post addition added

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/",function(req, res){
     res.redirect("/blogs");
 });
+
+
+
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -34,6 +37,23 @@ app.get("/blogs", function(req, res){
         }
     });
 });
+
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/blogs", function(req,res){
+    Blog.create(req.body.blog, function(err,newBlog){
+        if(err){
+            res.render("new");
+        } else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+
+
 
 
 app.listen(3000,function(){
