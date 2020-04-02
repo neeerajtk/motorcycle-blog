837312a1b20dcd9df8216836590cf231e754976f
neeerajtk <neerajtk560@gmail.com>
2020-04-02 23:43:15 +0530

    blog delete operation added

--- app.js
+++ app.js
@@ -84,6 +84,17 @@ app.put("/blogs/:id", function(req, res){
     })
 });
 
+app.delete("/blogs/:id", function(req,res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    })
+});
+
 app.listen(3000,function(){
 	console.log("Motoblog app started");
 });
