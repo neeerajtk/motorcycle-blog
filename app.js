39cd1b29142d22d7f328f83d869e320d3bf87992
neeerajtk <neerajtk560@gmail.com>
2020-04-02 12:42:14 +0530

    basic layout added

--- app.js
+++ app.js
@@ -26,7 +26,13 @@ app.get("/",function(req, res){
     res.redirect("/blogs");
 });
 app.get("/blogs", function(req, res){
-    res.render("index");
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR ! ");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
 });
 
 
